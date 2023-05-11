fc8da8414348113150b5e3076033cdf918477f77
Youssef Agnaou <Agnaou004@gmail.com>
2023-05-11 21:42:49 +0100

    mandatory is done, I GUESS!

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,31 +1,7 @@
 #!/bin/sh
-echo  "CREATE DATABASE $MARIA_NAME;" >> init.sql
-echo "GRANT ALL PRIVILEGES ON $MARIA_NAME.* TO '$MARIA_USER'@'%' IDENTIFIED BY '$MARIA_PASSWORD';">> init.sql
-echo " FLUSH PRIVILEGES;">> init.sql
+
+echo "CREATE DATABASE $MARIA_NAME;" >> init.sql
+echo "GRANT ALL PRIVILEGES ON $MARIA_NAME.* TO '$MARIA_USER'@'%' IDENTIFIED BY '$MARIA_PASSWORD';" >> init.sql
+echo "FLUSH PRIVILEGES;" >> init.sql
+
 mysqld --user=mysql --init-file=/tmp/init.sql
-
-
-# Create the database and grant privileges to the user
-
-
-# sleep 20000
-# If not set up yet
-# if [ $? -ne 0 ]; then
-
-# 	# sed -i 's/skip-networking/#skip-networking/g' /etc/my.cnf.d/mariadb-server.cnf
-#     # sed -i 's/#bind-address/bind-address/g' /etc/my.cnf.d/mariadb-server.cnf
-
-# 	# if ! mysqladmin --wait=30 ping; then
-#     #   printf "MariaDB Daemon Unreachable\n"
-#     #   exit 1
-#   	# fi
-
-# 	mysql -u root --password=password -e "CREATE DATABASE ${MARIA_NAME};"
-# 	mysql -u root --password=password -e "GRANT ALL PRIVILEGES ON ${MARIA_NAME}.* TO '${MARIA_USER}'@'%' IDENTIFIED BY '${MARIA_PASSWORD}';"
-# 	mysql -u root --password=password -e "FLUSH PRIVILEGES;"
-
-# 	# sleep 20000
-
-# 	# eval "echo \"$(cat /tmp/query.sql)\"" | mariadb
-#     # pkill mariadb
-# fi
--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -5,10 +5,14 @@ if [ ! -d "/var/www/html" ]; then
 fi
 
 cd /var/www/html
+
 sleep 5
-  wp core download --allow-root --version=5.8.1 --locale=en_US
-  wp config create --allow-root --dbname=${WP_NAME} --dbuser=${WP_USER} --dbpass=${WP_PASSWORD} --dbhost=${WP_HOST} 
-#   wp db create --allow-root
-  wp core install --allow-root --url=${WP_URL} --title=${WP_TITLE} --admin_user=${WP_ADMIN_USER} --admin_password=${WP_ADMIN_PASSWORD} --admin_email=${WP_ADMIN_EMAIL}
+
+wp core download --allow-root --version=5.8.1 --locale=en_US
+wp config create --allow-root --dbname=${WP_NAME} --dbuser=${WP_ADMIN_USER} --dbpass=${WP_PASSWORD} --dbhost=${WP_HOST}
+wp core install --allow-root --url=${WP_URL} --title=${WP_TITLE} --admin_user=${WP_ADMIN_USER} --admin_password=${WP_ADMIN_PASSWORD} --admin_email=${WP_ADMIN_EMAIL}
+
+# create a new wordpress user
+wp user create "${WP_USER}" "${WP_EMAIL}" --user_pass="${WP_PASSWORD}" --role=author
 
 exec php-fpm7 -F -R 
